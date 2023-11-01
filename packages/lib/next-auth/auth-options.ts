9847b7666b9847b26a80f112f17bf9aa18ab2736
citom <84733106+citom@users.noreply.github.com>
2023-11-01 08:38:51 +0100

    Azure Fix

--- packages/lib/next-auth/auth-options.ts
+++ packages/lib/next-auth/auth-options.ts
@@ -73,7 +73,7 @@ export const NEXT_AUTH_OPTIONS: AuthOptions = {
 
       profile(profile) {
         return {
-          id: Number(profile.sub),
+          id: String(profile.sub),
           name: profile.name || `${profile.given_name} ${profile.family_name}`.trim(),
           email: profile.email,
         };
